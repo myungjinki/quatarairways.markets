fdeb0c55bfec83b20f75e8d5ea18dd24243db020
Myungjin Ki <hello@mjki.dev>
2024-12-04 15:58:31 +0900

    Style login page for mobile (#18)

---
+++ app/(auth)/log-in/components/login-form.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import Button from "@/app/components/button";
+import Input from "@/app/components/input";
+import { handleForm } from "../actions";
+
+export default function LoginForm() {
+  const [state, action] = useFormState(handleForm, null);
+
+  return (
+    <form action={action} className="flex flex-col items-start w-full gap-4 pb-8 border-b-2">
+      <Input name="email" type="email" placeholder="Email" errors={state?.errors?.fieldErrors.email} />
+      <Input name="password" type="password" placeholder="Password" errors={state?.errors?.fieldErrors.password} />
+      <Button>Log in</Button>
+    </form>
+  );
+}
--- app/components/button.tsx
+++ app/components/button.tsx
@@ -8,7 +8,11 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export default function Button(props: ButtonProps) {
   const { pending } = useFormStatus();
   return (
-    <button {...props} disabled={pending}>
+    <button
+      {...props}
+      disabled={pending}
+      className="flex justify-center w-full p-4 font-bold text-white rounded-full bg-rose-800"
+    >
       {pending ? "Loading..." : props.children}
     </button>
   );
