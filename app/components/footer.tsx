f85973b1550de346c0dbb2377928bb28d772888a
myungjinki <hello@mjki.dev>
2024-12-05 12:42:35 +0900

    Modify footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center p-8">
-      <div className="flex items-center">&copy;2024 Quatar Airways Markets. All rights reserved </div>
+      <div className="flex flex-col items-center md:flex-row">
+        <span>Quatar Airways Markets</span>
+        <span className="hidden md:block">&nbsp;</span>
+        <span>&copy;2024. All rights reserved</span>
+      </div>
     </footer>
   );
 }
